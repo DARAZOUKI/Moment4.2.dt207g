b88e3d4a2ee33fbf1b41d7fce9d84f802465aabb
DARAZOUKI <Aussama@chalmers.se>
2024-05-30 00:07:18 +0200

    edit1

--- web-service/server.js
+++ web-service/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const cors = require('cors'); // Import cors package
+const cors = require('cors');
 const dotenv = require('dotenv');
 
 dotenv.config();
@@ -10,18 +10,24 @@ dotenv.config();
 const app = express();
 
 // Middleware
-
 app.use(cors());
 app.use(express.json());
 
 // Debugging: Log the MONGO_URI
 console.log('MONGO_URI:', process.env.MONGO_URI);
+
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  
-})
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => {
+        console.error('MongoDB connection error:', err);
+        process.exit(1); 
+    });
+
+// Define a root route
+app.get('/', (req, res) => {
+    res.send('Welcome to the server! The server is running successfully.');
+});
 
 // Routes
 app.use('/api/auth', require('./routes/auth'));
