2dfc38043b382fd6dd84b4eb7e123b871d4ec9ca
DARAZOUKI <Aussama@chalmers.se>
2024-05-30 00:17:26 +0200

    edit the root route

--- web-service/server.js
+++ web-service/server.js
@@ -12,6 +12,7 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.static(__dirname + '/src'));
 
 // Debugging: Log the MONGO_URI
 console.log('MONGO_URI:', process.env.MONGO_URI);
@@ -24,7 +25,9 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
         process.exit(1); 
     });
 
-
+    app.get('/', (req, res) => {
+        res.sendFile(__dirname + '/src/index.html');
+    });
 
 // Routes
 app.use('/api/auth', require('./routes/auth'));
